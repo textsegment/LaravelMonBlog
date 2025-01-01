6446e1cea67a678e96de0e74912fe38d8034aff2
TextSegment <dev@textsegment.com>
2025-01-01 15:26:55 +0100

    Add supprimer un article

--- resources/views/supprimerUnArticle.blade.php
+++ resources/views/supprimerUnArticle.blade.php
@@ -1,7 +1,12 @@
 @include('header')
 
 <div>
-    {{ $article }}
+    Voulez-vous supprimer l'article : <b>{{ $article->titre }}</b>?
+    <form method="post">
+        @csrf 
+        <a href="/">Non</a>
+        <input value="Oui" type="submit"/>
+    </form>
 </div>
 
 @include('footer')
