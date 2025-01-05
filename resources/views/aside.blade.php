8b3b859fdc30dd057a584739ec812783236c0805
TextSegment <dev@textsegment.com>
2025-01-05 11:41:56 +0100

    Update aside

--- resources/views/aside.blade.php
+++ resources/views/aside.blade.php
@@ -1,7 +1,11 @@
 <aside>
-    <ul>
-        <li><a href="/login/">S'identifier</a></li>
-        <li><a href="/register/">Créer un utilisateur</a></li>
-        <li><a href="/ajouterUnArticle/">Publier un article</a></li>
+    <ul> 
+    @guest
+    <li><a href="/login/">S'identifier</a></li>
+    <li><a href="/register/">Créer un utilisateur</a></li>
+    @endguest
+    @auth
+    <li><a href="/ajouterUnArticle/">Publier un article</a></li>
+    @endauth
     </ul>
 </aside>
