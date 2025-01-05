4567f4489496748a3c768e16bc0161292cd18c34
TextSegment <dev@textsegment.com>
2025-01-05 12:25:55 +0100

    update style for add article

--- resources/views/ajouterUnArticle.blade.php
+++ resources/views/ajouterUnArticle.blade.php
@@ -4,14 +4,18 @@
     <form method="post">
         @csrf
  
-        <table>
+        <table id="ajouterunarticle">
             <tr>
-                <td><label>Title de l'article</label></td>
-                <td><input type="text" name="titre" /></td>
+                <td>
+                    <label>Title de l'article</label>
+                    <input required id="titre" type="text" name="titre" />
+                </td>
             </tr>
             <tr>
-                <td><label>Contenu de l'article</label></td>
-                <td><input type="text" name="contenu" /></td>
+                <td>
+                    <label>Contenu de l'article</label>
+                    <textarea required id="contenu" type="text" name="contenu"></textarea>
+                </td>
             </tr>
         </table>
         <input type="submit" value="Envoyer" />
